9b6c731cb52f9d0fae40b16e38a1c0b064b7ee4b
Brian.D.Jamison <Brian.D.Jamison@noaa.gov>
2020-06-16 19:55:41 +0000

    Modifications to RH entries in default_specs.py

---
+++ plot_example.py
@@ -0,0 +1,27 @@
+#pylint: disable=invalid-name
+
+''' Example script for creating a figure with the adb_graphics package. '''
+
+import matplotlib.pyplot as plt
+
+import adb_graphics.datahandler.grib as grib
+import adb_graphics.figures.maps as maps
+
+filename = '../data/wrfnat_hrconus_12.grib2'
+airports = 'static/Airports_locs.txt'
+
+var = grib.UPPData(filename=filename,
+                   level='2ds',
+                   short_name='temp',
+                   )
+#contour_var = grib.UPPData(filename=filename,
+#                      level='500mb',
+#                      short_name='gh',
+#                      )
+
+contour_var = None
+
+fig, ax = plt.subplots(1, 1, figsize=(12, 12))
+m = maps.Map(airports, ax, corners=var.corners, region='fv3')
+dm = maps.DataMap(field=var, contour_field=contour_var, map_=m)
+dm.draw(show=True)
