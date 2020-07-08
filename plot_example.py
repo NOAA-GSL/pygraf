f8f76515cd5f26e170ff95abc78d1bb40dc1be3d
Christina Holt <Christina.Holt@noaa.gov>
2020-07-08 21:54:36 +0000

    Updating plot_example to work with new classes.

--- plot_example.py
+++ plot_example.py
@@ -10,14 +10,14 @@ import adb_graphics.figures.maps as maps
 filename = '../data/wrfnat_hrconus_12.grib2'
 airports = 'static/Airports_locs.txt'
 
-var = grib.UPPData(filename=filename,
-                   level='2ds',
-                   short_name='temp',
-                   )
-contour_var = grib.UPPData(filename=filename,
-                      level='500mb',
-                      short_name='gh',
-                      )
+var = grib.fieldData(filename=filename,
+                     level='2ds',
+                     short_name='temp',
+                     )
+contour_var = grib.fieldData(filename=filename,
+                             level='500mb',
+                             short_name='gh',
+                             )
 
 #contour_var = None
 
