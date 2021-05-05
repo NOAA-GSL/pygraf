2f6ed64a48e97940f98a8147abe841717fef4c9b
Christina Holt <56881914+christinaholtNOAA@users.noreply.github.com>
2021-05-05 12:24:07 -0600

    Fixes needed for run-total graphics in HRRR (#83)
    * Mods for renaming more generically.
    
    * Working for all_leads 0 - 6 hrrr.
    
    * Cleaning up debug statements
    
    * Cleanup, lint, test.

--- adb_graphics/datahandler/gribfile.py
+++ adb_graphics/datahandler/gribfile.py
@@ -4,6 +4,8 @@
 Classes that load grib files.
 '''
 
+from functools import lru_cache
+
 import xarray as xr
 
 class GribFile():
@@ -67,6 +69,24 @@ class GribFiles():
         self.contents = self._load(filenames)
 
     @staticmethod
+    def free_fcst_names(ds):
+
+        ''' Given an opened dataset, return a dict of original variable names
+        (key) and the desired name (value) '''
+
+        ret = {}
+        for var in ds.variables:
+            suffix = var.split('_')[-1]
+
+            # Because there doesn't seem to be another way to know....
+            needs_renaming = var.split('_')[0] not in ['ASNOW', 'FRZR']
+
+            if suffix in ['max', 'min', 'acc', 'avg'] and needs_renaming:
+                new_suffix = f'{suffix}1h'
+                ret[var] = var.replace(suffix, new_suffix)
+        return ret
+
+    @staticmethod
     def _get_grid_suffix(filenames):
 
         ''' Return the suffix of the first variable with 4 sections (split on _)
@@ -92,34 +112,30 @@ class GribFiles():
 
         all_leads = [] if filenames is None else [self.contents]
         filenames = self.filenames if filenames is None else filenames
-        var_names = self.variable_names
 
-        # 0h and 1h forecast variables are named like the keys in var_names,
-        # while the free forecast hours are named like the values in var_names.
-        # Need to do a bit of cleanup to get them into a single datastructure.
-        names = {
-            '01fcst': var_names,
-            'free_fcst': var_names.values(),
-            }
-
-        for fcst_type, vnames in names.items():
+        # 0h and 1h accumulated forecast variables are named differently than
+        # the rest of the forecast hours. Rename those accumulated variables if
+        # needed.
+        for fcst_type in ['01fcst', 'free_fcst']:
 
             if filenames.get(fcst_type):
                 for filename in filenames.get(fcst_type):
                     print(f'Loading grib2 file: {fcst_type}, {filename}')
 
+                # Rename variables to match free forecast variables
+                dataset = xr.open_mfdataset(
+                    filenames[fcst_type],
+                    **self.open_kwargs,
+                    )
+
                 if fcst_type == '01fcst':
-                    if filenames.get(fcst_type):
-                        # Rename variables to match free forecast variables
-                        all_leads.append(xr.open_mfdataset(
-                            filenames[fcst_type],
-                            **self.open_kwargs(vnames),
-                            ).rename_vars(vnames))
-                else:
-                    all_leads.append(xr.open_mfdataset(
-                        filenames[fcst_type],
-                        **self.open_kwargs(vnames),
-                        ))
+                    print(f'RENAMING VARIABLES:')
+                    renaming = self.free_fcst_names(dataset)
+                    for old_name, new_name in renaming.items():
+                        print(f'  {old_name:>30s}  -> {new_name}')
+                    dataset = dataset.rename_vars(renaming)
+
+                all_leads.append(dataset)
 
         ret = xr.combine_nested(all_leads,
                                 compat='override',
@@ -130,7 +146,9 @@ class GribFiles():
 
         return ret
 
-    def open_kwargs(self, vnames):
+    @property
+    @lru_cache()
+    def open_kwargs(self):
 
         ''' Defines the key word arguments used by the various calls to XArray
         open_mfdataset '''
@@ -141,54 +159,6 @@ class GribFiles():
             compat='override',
             concat_dim=list(self.coord_dims.keys())[0],
             coords='minimal',
-            data_vars=vnames,
             engine='pynio',
             lock=False,
             )
-
-    @property
-    def variable_names(self):
-
-        '''
-        Defines the variable name transitions that need to happen for each
-        model to combine along forecast hours.
-
-        Keys are original variable names, values are the updated variable names.
-
-        Choosing to update the 0 and 1 hour forecast variables to the longer
-        lead times for efficiency's sake.
-        '''
-
-        grid = self.grid_suffix
-        names = {
-            'hrrr': {
-                f'REFC_P0_L10_{grid}': f'REFC_P0_L10_{grid}',
-                f'MXUPHL_P8_2L103_{grid}_max': f'MXUPHL_P8_2L103_{grid}_max1h',
-                f'UGRD_P0_L103_{grid}': f'UGRD_P0_L103_{grid}',
-                f'VGRD_P0_L103_{grid}': f'VGRD_P0_L103_{grid}',
-                f'WEASD_P8_L1_{grid}_acc': f'WEASD_P8_L1_{grid}_acc1h',
-                f'APCP_P8_L1_{grid}_acc': f'APCP_P8_L1_{grid}_acc1h',
-                f'PRES_P0_L1_{grid}': f'PRES_P0_L1_{grid}',
-                f'VAR_0_7_200_P8_2L103_{grid}_min': f'VAR_0_7_200_P8_2L103_{grid}_min1h',
-                },
-            'rap': {
-                f'REFC_P0_L10_{grid}': f'REFC_P0_L10_{grid}',
-                f'UGRD_P0_L103_{grid}': f'UGRD_P0_L103_{grid}',
-                f'VGRD_P0_L103_{grid}': f'VGRD_P0_L103_{grid}',
-                f'WEASD_P8_L1_{grid}_acc': f'WEASD_P8_L1_{grid}_acc1h',
-                f'APCP_P8_L1_{grid}_acc': f'APCP_P8_L1_{grid}_acc1h',
-                f'PRES_P0_L1_{grid}': f'PRES_P0_L1_{grid}',
-                },
-            'rrfs': {
-                f'REFC_P0_L200_{grid}': f'REFC_P0_L200_{grid}',
-                f'MXUPHL_P8_2L103_{grid}_max': f'MXUPHL_P8_2L103_{grid}_max1h',
-                f'UGRD_P0_L103_{grid}': f'UGRD_P0_L103_{grid}',
-                f'VGRD_P0_L103_{grid}': f'VGRD_P0_L103_{grid}',
-                f'WEASD_P8_L1_{grid}_acc': f'WEASD_P8_L1_{grid}_acc1h',
-                f'APCP_P8_L1_{grid}_acc': f'APCP_P8_L1_{grid}_acc1h',
-                f'PRES_P0_L1_{grid}': f'PRES_P0_L1_{grid}',
-                f'VAR_0_7_200_P8_2L103_{grid}_min': f'VAR_0_7_200_P8_2L103_{grid}_min1h',
-                },
-            }
-
-        return names[self.model]
