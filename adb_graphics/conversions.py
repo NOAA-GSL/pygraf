950d7e303587aa6280a7a50e3cb58595333f42ba
Christina Holt <Christina.Holt@noaa.gov>
2021-09-24 20:25:06 +0000

    Don't transform to numpy arrays on conversion.

--- adb_graphics/conversions.py
+++ adb_graphics/conversions.py
@@ -11,19 +11,19 @@ def k_to_c(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from Kelvin to Celsius '''
 
-    return np.asarray(field) - 273.15
+    return field - 273.15
 
 def k_to_f(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from Kelvin to Farenheit '''
 
-    return (np.asarray(field) - 273.15) * 9/5 + 32
+    return (field - 273.15) * 9/5 + 32
 
 def kgm2_to_in(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from kg per m^2 to inches '''
 
-    return np.asarray(field) * 0.03937
+    return field * 0.03937
 
 def magnitude(a: np.ndarray, b: np.ndarray, **kwargs) -> np.ndarray:
 
@@ -35,63 +35,63 @@ def m_to_dm(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from meters to decameters '''
 
-    return np.asarray(field) / 10.
+    return field / 10.
 
 def m_to_in(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from meters to inches '''
 
-    return np.asarray(field) * 39.3701
+    return field * 39.3701
 
 def m_to_kft(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from meters to kilofeet '''
 
-    return np.asarray(field) / 304.8
+    return field / 304.8
 
 def m_to_mi(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from meters to miles '''
-    return np.asarray(field) / 1609.344
+    return field / 1609.344
 
 def ms_to_kt(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from m s-1 to knots '''
 
-    return np.asarray(field) * 1.9438
+    return field * 1.9438
 
 def pa_to_hpa(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from Pascals to hectopascals '''
 
-    return np.asarray(field) / 100.
+    return field / 100.
 
 def percent(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from values between 0 - 1 to percent '''
 
-    return np.asarray(field) * 100.
+    return field * 100.
 
 def to_micro(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Convert field to micro '''
 
-    return np.asarray(field) * 1E6
+    return field * 1E6
 
 def vvel_scale(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Scale vertical velocity for plotting  '''
 
-    return np.asarray(field) * -10
+    return field * -10
 
 def vort_scale(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Scale vorticity for plotting  '''
 
-    return np.asarray(field) / 1E-05
+    return field / 1E-05
 
 def weasd_to_1hsnw(field: np.ndarray, **kwargs) -> np.ndarray:
 
     ''' Conversion from snow wter equiv to snow (10:1 ratio) '''
 
-    return np.asarray(field) * 10.
+    return field * 10.
