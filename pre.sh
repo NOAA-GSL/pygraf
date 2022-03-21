86e087d3f82fadb3d16bef1691bc1d83e83d8350
Christina Holt <Christina.Holt@noaa.gov>
2022-03-21 21:49:11 +0000

    Set modules to exact versions. Default not supported.

--- pre.sh
+++ pre.sh
@@ -3,9 +3,9 @@
 module purge
 
 module use -a /contrib/miniconda3/modulefiles
-module load miniconda3
+module load miniconda3/4.5.12
 conda activate pygraf
 
-module load wgrib2
+module load wgrib2/0.1.9.6a
 
 module list
