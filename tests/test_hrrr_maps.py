b0e7b48db8d26055f4687e3a7146146962d6b70b
brendan-ryan <brendanmryan@gmail.com>
2023-04-25 15:50:21 -0600

    Changed 'args' string to remove 'create_graphics' from front

--- tests/test_hrrr_maps.py
+++ tests/test_hrrr_maps.py
@@ -9,7 +9,7 @@ output_loc = os.environ.get("output_loc")
 
 def test_parse_args():
     ''' Test function for existence of HRRR 12-hour accumulated maps. '''
-    args = ['create_graphics', 'maps', '-d', str(data_loc), '-f', '0 12 1', '-o', str(output_loc),\
+    args = ['maps', '-d', str(data_loc), '-f', '0 12 1', '-o', str(output_loc),\
          '-s', '2021052315', '--file_tmpl', 'hrrr.t00z.wrfprsf{FCST_TIME:02d}.grib2', \
             '--images', 'image_lists/hrrr_test.yml', 'hourly', '--all_leads', '--file_type=prs']
     create_graphics(args)
