ea35f91ae1e7a4aaec9270e642daa29a1dd78b0b
brendan-ryan <brendanmryan@gmail.com>
2023-04-25 13:50:14 -0600

    Skeleton for test script

---
+++ tests/test_hrrr_maps.py
@@ -0,0 +1,18 @@
+#pylint: disable=unused-variable
+''' Tests for create_graphics driver '''
+from create_graphics import create_graphics
+import os
+
+data_loc = os.environ.get("data_loc")
+output_loc = os.environ.get("output_loc")
+
+def test_parse_args():
+    ''' Test function for existence of HRRR 12-hour accumulated maps. '''
+    args = ['create_graphics', 'maps', '-d', '{data_loc}', '-f', '0 12 1', '-o', '{output_loc}',\
+         '-s', '2021052315', '--file_tmpl', 'hrrr.t00z.wrfprsf{FCST_TIME:02d}.grib2', \
+            '--images', './image_lists/hrrr_test.yml', 'hourly', '--all_leads', '--file_type=prs']
+    create_graphics(args)
+    assert test_args.graphic_type == 'skewts'   # this of course will fail. Holdover from initial test. To be replaced.
+
+def test_nonsense():
+    assert 5 == 5
