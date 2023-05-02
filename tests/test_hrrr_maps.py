74f79b7c662277fd4116e2afd3178c96bd12ac09
brendan-ryan <brendanmryan@gmail.com>
2023-05-01 18:51:13 -0600

    Added test_parse_args and path check in test_existence

--- tests/test_hrrr_maps.py
+++ tests/test_hrrr_maps.py
@@ -7,19 +7,28 @@ import os.path
 data_loc = os.environ.get("data_loc")
 output_loc = os.environ.get("output_loc")
 
+
 def test_parse_args():
-    ''' Test function for existence of HRRR 12-hour accumulated maps. '''
+    ''' Test parse_args for basic parsing success. '''
+    args = ['create_graphics', 'maps', '-d', str(data_loc), '-f', '0', '12', '1', '-o', str(output_loc),\
+         '-s', '2021052315', '--file_tmpl', 'hrrr.t00z.wrfprsf{FCST_TIME:02d}.grib2', \
+            '--images', './image_lists/hrrr_test.yml', 'hourly', '--all_leads', '--file_type=prs']
+    test_args = parse_args(args)
+    assert test_args.graphic_type == 'maps'
+
+
+def test_maps():
+    ''' Test function to build HRRR 12-hour accumulated maps. '''
     args = ['maps', '-d', str(data_loc), '-f', '0', '12', '1', '-o', str(output_loc),\
          '-s', '2021052315', '--file_tmpl', 'hrrr.t00z.wrfprsf{FCST_TIME:02d}.grib2', \
             '--images', './image_lists/hrrr_test.yml', 'hourly', '--all_leads', '--file_type=prs']
     create_graphics(args)
-    assert test_args.graphic_type == 'skewts'   # this of course will fail. Holdover from initial test. To be replaced.
 
-def test_nonsense():
-    assert 5 == 5
 
 def test_existence():
-    f0 = '/202303150100'
-    path = output_loc + f0
-    p0 = os.path.isdir(path)
+    ''' Test function to check for existence of output folders and files. '''
+    folder_0 = '/202303150100'
+    full_path_0 = output_loc + folder_0
+    p0 = os.path.isdir(full_path_0)
+    print("Full path:", flul_path_0)
     assert p0 == True
