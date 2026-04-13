d1d926736395c3e27aa234594ca75e84490679d8
Christina Holt <Christina.Holt@noaa.gov>
2026-04-13 15:02:44 +0000

    Plotting ceilexp for HRRR

--- pre.sh
+++ pre.sh
@@ -2,8 +2,15 @@
 
 module purge
 
-module use -a /contrib/miniconda3/modulefiles
-module load miniconda3/4.12.0
-conda activate pygraf
+
+if [[ $(hostname) == u* ]] ; then
+  module use -a /contrib/miniconda/modulefiles
+  module load miniconda/25.3.1
+  conda activate pygraf_rap
+else
+  module use -a /contrib/miniconda3/modulefiles
+  module load miniconda3/4.12.0
+  conda activate pygraf
+fi
 
 module list
